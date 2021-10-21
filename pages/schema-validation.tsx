05bdc14bd3cc87c64d83851c8b956bc94a94e6f5
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-10-22 02:29:05 +0900

    Add 2 examples

---
+++ pages/schema-validation.tsx
@@ -0,0 +1,40 @@
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+interface IFormInputs {
+    firstName: string
+    age: number
+}
+
+const schema = yup
+    .object({
+        firstName: yup.string().required(),
+        age: yup.number().positive().integer().required(),
+    })
+    .required()
+
+const App = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<IFormInputs>({
+        resolver: yupResolver(schema),
+    })
+    const onSubmit = (data: IFormInputs) => console.log(data)
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register('firstName')} />
+            <p>{errors.firstName?.message}</p>
+
+            <input {...register('age')} />
+            <p>{errors.age?.message}</p>
+
+            <input type='submit' />
+        </form>
+    )
+}
+
+export default App
