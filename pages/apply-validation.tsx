11a70791d5d8938cc8a6473f527d49ba843f63c5
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-10-22 01:16:33 +0900

    サンプルから3例追加

---
+++ pages/apply-validation.tsx
@@ -0,0 +1,26 @@
+//Example code from https://react-hook-form.com/get-started#applyvalidation
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+interface IFormInput {
+    firstName: string
+    lastName: string
+    age: number
+}
+
+const ApplyValidation = () => {
+    const { register, handleSubmit } = useForm<IFormInput>()
+    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+                {...register('firstName', { required: true, maxLength: 20 })}
+            />
+            <input {...register('lastName', { pattern: /^[A-Za-z]+$/i })} />
+            <input type='number' {...register('age', { min: 18, max: 99 })} />
+            <input type='submit' />
+        </form>
+    )
+}
+
+export default ApplyValidation
