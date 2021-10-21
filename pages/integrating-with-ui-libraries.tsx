5c5d443ffff602140e865053f5a665df733844a6
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-10-22 02:07:51 +0900

    material-uiとの組み合わせ

---
+++ pages/integrating-with-ui-libraries.tsx
@@ -0,0 +1,45 @@
+import Select from 'react-select'
+import { useForm, Controller, SubmitHandler } from 'react-hook-form'
+import Input from '@material-ui/core/Input'
+
+interface IFormInput {
+    firstName: string
+    lastName: string
+    iceCreamType: { label: string; value: string }
+}
+
+const App = () => {
+    const { control, handleSubmit } = useForm<IFormInput>()
+
+    const onSubmit: SubmitHandler<IFormInput> = (data) => {
+        console.log(data)
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+                name='firstName'
+                control={control}
+                defaultValue=''
+                render={({ field }) => <Input {...field} />}
+            />
+            <Controller
+                name='iceCreamType'
+                control={control}
+                render={({ field }) => (
+                    <Select
+                        {...field}
+                        options={[
+                            { value: 'chocolate', label: 'Chocolate' },
+                            { value: 'strawberry', label: 'Strawberry' },
+                            { value: 'vanilla', label: 'Vanilla' },
+                        ]}
+                    />
+                )}
+            />
+            <input type='submit' />
+        </form>
+    )
+}
+
+export default App
